de5748060dba6f018fdcde7ab119d193bd4c443c
Murphy2305 <kpshekhawat678@gmail.com>
2025-03-09 00:28:43 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -41,14 +41,27 @@ const data1 =
 
 
 
-app.get('/aa',(req,res)=>{
-    res.json(data1);
-})
-
 app.use('/user',userRoute);
 app.use('/chat',chatRoute);
 app.use('/message', messageRoute);
 
+
+
+
+//DEPLOYMENT CODE IS HERE
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 
 app.use(notFound);
