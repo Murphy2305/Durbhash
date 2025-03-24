5d84d74b8916234acf4c1d5f253af95781d26a42
Murphy2305 <kpshekhawat678@gmail.com>
2025-03-25 00:46:32 +0530

    cors render

--- Server/server.js
+++ Server/server.js
@@ -59,7 +59,7 @@ const server = app.listen(PORT, () => {
 const io = require('socket.io')(server,{
     pingTimeout : 60000,
     cors:{
-        origin: "https://durbhash-7.vercel.app/",
+        origin: "https://durbhash-2.onrender.com/",
         methods: ["POST", "GET"],
   credentials: true
 
